6013cf128243c1126b10dc2683e3f0652b4570ce
Osunla Blessing <osunlab710@gmail.com>
2023-03-30 22:06:59 +0100

    hamburger

--- life.js
+++ life.js
@@ -1,8 +1,18 @@
 let ham = document.getElementById("ham");
 let show = document.getElementById("show");
+let cancel = document.getElementById("cancel-icon");
+let canBtn = document.getElementById("cancel-btn");
 
 ham.addEventListener("click", (e) => {
-  console.log("hello");
+  show.style.display = "flex";
+  ham.classList.add("disabled");
+  cancel.style.display = "block";
 
-  show.style.display = "block";
+  cancel.addEventListener("click", (e) => {
+    cancel.style.display = "none";
+    ham.style.display = "block";
+    show.style.display = "none";
+  });
+
+  return;
 });
