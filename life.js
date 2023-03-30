93c1fc90702ff174cd9c02141f7801336f4104aa
Osunla Blessing <osunlab710@gmail.com>
2023-03-30 15:28:12 +0100

    rounding up the hamburger

---
+++ life.js
@@ -0,0 +1,8 @@
+let ham = document.getElementById("ham");
+let show = document.getElementById("show");
+
+ham.addEventListener("click", (e) => {
+  console.log("hello");
+
+  show.style.display = "block";
+});
