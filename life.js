438650a421c63e9c97e73ae581b4711f31cff069
Osunla Blessing <osunlab710@gmail.com>
2023-03-31 10:53:35 +0100

    .

--- life.js
+++ life.js
@@ -2,13 +2,13 @@ let ham = document.getElementById("ham");
 let show = document.getElementById("show");
 let cancel = document.getElementById("cancel-icon");
 let canBtn = document.getElementById("cancel-btn");
+let toggleMenu = document.getElementsByClassName("hide-links");
 
 ham.addEventListener("click", (e) => {
   show.style.display = "flex";
-  // show.style.transform = "translate(10px, 0)";
-  // show.style.transition = "all 5s ease";
   ham.style.display = "none";
   cancel.style.display = "block";
+  return toggleMenu;
 });
 cancel.addEventListener("click", (e) => {
   cancel.style.display = "none";
